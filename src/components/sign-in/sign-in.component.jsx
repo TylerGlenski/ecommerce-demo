80409b7002ad912be931c37d44d07fc6f84700b6
Tyler Glenski <tglenski1@gmail.com>
2019-12-26 13:49:29 -0600

    Update: added sign-in with email/password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 import './sign-in.styles.scss';
@@ -18,11 +18,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = e => {
-        e.preventdefault();
+    handleSubmit = async e => {
+        e.preventDefault();
+        
+        const { email, password } = this.state;
 
-        this.setState({ email:'', password: '' });
-    }
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     handleChange = e => {
         const { value, name } = e.target;
