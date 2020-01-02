1eafc5909aae6d57eab210b16bcc113b22ac6536
Tyler Glenski <tglenski1@gmail.com>
2020-01-02 17:09:23 -0600

    integrated redux-persist and cart reducer persistance

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,24 @@
 import { combineReducers } from 'redux';
-// reducer imports
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers(
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    // reducers you want to persist add to whitelist
+    // user is already persisted by firebase, no need to whitelist
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers(
     {
         user: userReducer,
         cart: cartReducer
     }
 );
+
+export default persistReducer(persistConfig, rootReducer);
