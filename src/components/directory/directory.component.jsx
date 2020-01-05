e24f5e1d51045302e40bb6a5550cb2e8f5e2e30f
Tyler Glenski <tglenski1@gmail.com>
2020-01-05 12:45:48 -0600

    moved directory & shop.data into own reducer, created respective selectors and updated components with new redux flow for directory and shop.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,63 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 
-class Directory extends React.Component {
-    constructor() {
-        super();
-
-        this.state = { 
-            sections: 
-            [
-                {
-                    title: 'earrings',
-                    imageUrl: 'https://i.imgur.com/DvOFmW3.jpg',
-                    id: 1,
-                    linkUrl: 'earrings'
-                },
-                {
-                    title: 'necklaces',
-                    imageUrl: 'https://i.imgur.com/E1H2aIT.jpg?1',
-                    id: 2,
-                    linkUrl: ''
-                },
-                {
-                    title: 'bracelets',
-                    imageUrl: 'https://i.imgur.com/eLWdtei.jpg',
-                    id: 3,
-                    linkUrl: ''
-
-                },
-                {
-                    title:'kids',
-                    imageUrl: 'https://ae01.alicdn.com/kf/HTB1HJoTe41YBuNjy1zcq6zNcXXao/Menglina-Acrylic-Cartoon-Hairbow-Horse-Earrings-For-Children-Baby-Horse-Resin-Dangle-Earrings-Girls-Fashion-Earrings.jpg',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: ''
-                },
-                {
-                    title:'mens',
-                    imageUrl:'https://i.imgur.com/gt6EQg0.jpg',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: ''
-                }
-            ]
-        }
-    }
-
-    render(){
-        return(
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({ id, ...otherProps }) => (
-                        <MenuItem key={id} {...otherProps}/>    
-                ))}
-            </div>
-        );
-    }
+const Directory = ({ sections }) => {
+   return(
+        <div className='directory-menu'>
+            {
+                sections.map(({ id, ...otherProps }) => (
+                    <MenuItem key={id} {...otherProps}/>    
+            ))}
+        </div>
+    ); 
 }
 
-export default Directory;
+export const mapStateToProps = createStructuredSelector(
+    {
+        sections: selectDirectorySections
+    }
+);
+
+export default connect(mapStateToProps)(Directory);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+//reducer imports
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -11,13 +14,15 @@ const persistConfig = {
     storage,
     // reducers you want to persist add to whitelist
     // user is already persisted by firebase, no need to whitelist
-    whitelist: ['cart']
+    whitelist: ['cart', 'directoryReducer']
 }
 
 const rootReducer = combineReducers(
     {
         user: userReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        directory: directoryReducer,
+        shop: shopReducer
     }
 );
 
